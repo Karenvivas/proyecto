3de541a1f47a2a79c55b91014eab0822d05cdf99
Herubyel <41125455+Herubyel@users.noreply.github.com>
2018-11-16 20:33:13 +0100

    Herubyel
    Modelos listos

--- app/activity.php
+++ app/activity.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class activity extends Model
 {
-    //
+
+    protected $table = 'activity';
+    protected $fillable = [
+        'nombre', 'descripcion', 'imagen'
+    ];
+
+    public function attractions(){
+        return $this->belongsToMany('App\attraction');
+    }
+
+    public function galleries(){
+        return $this->belongsToMany('App\gallery');
+    }
+
+    public function registries(){
+        return $this->belongsToMany('App\registry');
+    }
+
+    public function discount(){
+        return $this->hasOne('App\discount');
+    }
+
+    public function packages(){
+        return $this->belongsToMany('App\package');
+    }
 }
--- app/gallery.php
+++ app/gallery.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class gallery extends Model
 {
-    //
+    protected $table = 'gallery';
+    protected $fillable = [
+        'nombre'
+    ];
+
+    public function attractions(){
+        return $this->belongsToMany('App\attraction');
+    }
+
+    public function activities(){
+        return $this->belongsToMany('App\activity');
+    }
 }
